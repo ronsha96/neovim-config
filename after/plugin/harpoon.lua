1b357a0db1380d59d0c0ffa6335d5cc7ad6c0360
ronsha <rons@connecteam.com>
2022-12-26 12:07:23 +0200

    new harpoon keymaps

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -2,19 +2,24 @@ local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
 local map = vim.keymap.set
-local opts = { noremap = true, silent = true }
+local opts = { noremap = true, silent = false }
 
 map("n", "<leader>a", mark.add_file, opts)
+
 map("n", "<C-e>", ui.toggle_quick_menu, opts)
-map("n", "<C-m>", function()
+
+map("n", "<leader>m,", function()
 	ui.nav_file(1)
 end, opts)
-map("n", "<C-,>", function()
+
+map("n", "<leader>m.", function()
 	ui.nav_file(2)
 end, opts)
-map("n", "<C-.>", function()
+
+map("n", "<leader>m/", function()
 	ui.nav_file(3)
 end, opts)
-map("n", "<C-/>", function()
+
+map("n", "<leader>mm", function()
 	ui.nav_file(4)
 end, opts)
