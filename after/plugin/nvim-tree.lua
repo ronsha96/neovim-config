42548697150ac2c9538497f401d854d0ad56a54e
ronsha <rons@connecteam.com>
2023-01-01 10:36:48 +0200

    nvim tree config

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,5 +1,16 @@
 require("nvim-tree").setup({
+	git = {
+		enable = true,
+		ignore = true,
+		show_on_dirs = true,
+		timeout = 400,
+	},
+	sort_by = "case_sensitive",
+	view = {
+		adaptive_size = true,
+	},
 	renderer = {
+		group_empty = true,
 		full_name = true,
 	},
 })
