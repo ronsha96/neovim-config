0a2660d206d67a440b3f3c4358d051e862e4b72d
ronsha <rons@connecteam.com>
2023-01-02 11:51:25 +0200

    dont open tree by default

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -20,5 +20,3 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<leader>no", "<CMD>NvimTreeOpen<CR>", opts)
 vim.keymap.set("n", "<leader>nc", "<CMD>NvimTreeClose<CR>", opts)
 vim.keymap.set("n", "<leader>nf", "<CMD>NvimTreeFindFile<CR>", opts)
-
-vim.cmd([[ NvimTreeOpen ]]) -- open tree on startup
