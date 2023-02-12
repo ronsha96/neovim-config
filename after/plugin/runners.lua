3fc4ca3f930b5f783ce554449eba8891c8d8aca3
ronsha <rons@connecteam.com>
2023-02-12 13:12:42 +0200

    signs

--- after/plugin/runners.lua
+++ after/plugin/runners.lua
@@ -132,31 +132,6 @@ vim.keymap.set("n", "'ts", neotest.summary.toggle, opts)
 
 -- Debug
 
-local dap_breakpoint = {
-	error = {
-		text = "🟥",
-		texthl = "LspDiagnosticsSignError",
-		linehl = "",
-		numhl = "",
-	},
-	rejected = {
-		text = "",
-		texthl = "LspDiagnosticsSignHint",
-		linehl = "",
-		numhl = "",
-	},
-	stopped = {
-		text = "⭐️",
-		texthl = "LspDiagnosticsSignInformation",
-		linehl = "DiagnosticUnderlineInfo",
-		numhl = "LspDiagnosticsSignInformation",
-	},
-}
-
-vim.fn.sign_define("DapBreakpoint", dap_breakpoint.error)
-vim.fn.sign_define("DapStopped", dap_breakpoint.stopped)
-vim.fn.sign_define("DapBreakpointRejected", dap_breakpoint.rejected)
-
 require("nvim-dap-virtual-text").setup({
 	commented = true,
 })
